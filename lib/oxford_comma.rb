68de055b8aaadeaa43abe537778b7239e348ade4
Amanda Smith <ajs.amandasmith@gmail.com>
2022-07-20 15:32:39 -0500

    commit tree 5dd90ede935106d12f01df81291de425065dd9b6

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.size >= 3
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  else
+    array.join(' and ')
+  end
 end
+
+puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
